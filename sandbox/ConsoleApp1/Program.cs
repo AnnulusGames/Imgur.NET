5f75a5dab3be15c76f55448734c9fac8bcd170f2
AnnulusGames <annulusgames@gmail.com>
2024-06-29 13:59:43 +0900

    Add ConsoleApp1

---
+++ sandbox/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Imgur;
+
+using var client = new ImgurClient
+{
+    ClientId = args[0],
+    ClientSecret = args[1]
+};
+
+var bytes = await File.ReadAllBytesAsync("./image.png");
+
+var response = await client.Image.UploadAsync(new()
+{
+    Image = bytes
+});
+
+Console.WriteLine(response);
